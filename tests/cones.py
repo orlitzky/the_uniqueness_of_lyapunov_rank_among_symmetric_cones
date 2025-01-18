9c4ef42be6b423c580a6dbadaddb3687f1ae0d72
Michael Orlitzky <michael@orlitzky.com>
2025-01-18 00:37:28 -0500

    tests/cones.py: mention __hash__ collision testing in its docstring

--- tests/cones.py
+++ tests/cones.py
@@ -329,10 +329,11 @@ class SymmetricCone:
         (what we get when we serialize our cones), but I can't promise
         that two different tuples will never have the same hash, even
         if their components do not. So there is a small chance that we
-        load the wrong :class:`DirectSum` from the instance
-        cache. This would be a fun experiment to run on the cones
-        database at some point: there should be no duplicate cones in
-        the database, so there should be no duplicate hashes.
+        load the wrong :class:`DirectSum` from the instance cache.
+
+        NB: The ``verify-live-db.py`` script will check a large random
+        sample of cones in the database to ensure that none of their
+        hashes collide.
         """
         if self._hash is None:
             self._hash = hash(self.serialize())
